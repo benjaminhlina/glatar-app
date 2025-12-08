34421ee2c18ecba04a595728f2263e91a35ffede
benjaminhlina <benjamin.hlina@gmail.com>
2025-12-08 08:09:54 -0500

    Change host and port for AWS

--- modules/start_db_connection.R
+++ modules/start_db_connection.R
@@ -7,8 +7,9 @@
 # ----- create connection to database -----
 con <- DBI::dbConnect(RPostgres::Postgres(),
                       dbname = "NAAEDdb",
-                      host = "localhost",
-                      port = 5433,
+                      host = "bh-2.cluster-c1mqmq4qms1u.us-east-2.rds.amazonaws.com",
+                      port = 5432,
                       user = Sys.getenv("POSTGRES_USER"),
-                      password = Sys.getenv("POSTGRES_PASSWORD")
+                      password = Sys.getenv("POSTGRES_PASSWORD"),
+                      sslmode = "require"
                       )
