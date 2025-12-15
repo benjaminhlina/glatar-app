cde960c52faad6c0e77148c8d320a77ebd55a46e
benjaminhlina <benjamin.hlina@gmail.com>
2025-12-15 15:19:50 -0500

    update to sys.getenvionrment

--- modules/start_db_connection.R
+++ modules/start_db_connection.R
@@ -6,10 +6,10 @@
 
 # ----- create connection to database -----
 con <- DBI::dbConnect(RPostgres::Postgres(),
-                      dbname = "NAAEDdb",
-                      host = "bh-2.cluster-c1mqmq4qms1u.us-east-2.rds.amazonaws.com",
-                      port = 5432,
+                      dbname = Sys.getenv("POSTGRES_DB"),
+                      host = Sys.getenv("POSTGRES_HOST"),
+                      port = Sys.getenv("POSTGRES_PORT"),
                       user = Sys.getenv("POSTGRES_USER"),
-                      password = Sys.getenv("POSTGRES_PASSWORD"),
-                      sslmode = "require"
+                      password = Sys.getenv("POSTGRES_PASSWORD")
+                      # sslmode = "require"
                       )
