bf2c39f8418babdbca63b2fa5fd8f896ab5bccd9
benjaminhlina <benjamin.hlina@gmail.com>
2026-01-14 14:49:22 -0500

    Add in build info

--- modules/app_version.R
+++ modules/app_version.R
@@ -9,15 +9,41 @@ app_version_head <- function() {
           font-size: 12px;
           color: #888;
           z-index: 1000;
+          display: flex;
+          gap: 6px;
+          align-items: center;
+      }
+
+      #app-version .version {
+        background-color: #f4f4f4;
+        border-radius: 3px;
+        padding: 2px 6px;
+        font-weight: 600;
+        color: #444;
+      }
+
+      #app-version .build {
+        background-color: #e9ecef;
+        border-radius: 3px;
+        padding: 2px 6px;
+        color: #666;
         }
       "))
   )
 }
 
-app_version_label <- function(app_version) {
+
+
+app_version_label <- function(app_version, build_time = NULL) {
+  if (is.null(build_time)) {
+    build_time <- format(Sys.time(), "%Y-%m-%d %H:%M %Z")
+  }
+
   tags$div(
     id = "app-version",
-    paste("v", app_version)
+    tags$span(class = "version", paste0("v", app_version)),
+    tags$span(class = "build", paste0("Built: ", build_time))
   )
 }
+
 
