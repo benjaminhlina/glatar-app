f5143fccfdbbfae27e35a6128449ad5c58532a3b
benjaminhlina <benjamin.hlina@gmail.com>
2026-01-19 19:14:35 -0500

    Remove `show_plot_message()` and remove `empty_scatter_message()` and add `empty_plot()`. Will need to add `{ggtext}` and `element_markdown()`

--- modules/show_functions.R
+++ modules/show_functions.R
@@ -1,28 +1,36 @@
-show_plot_message <- function(msg) {
-  grid.newpage()
-  grid.text(
-    msg,
-    x = 0.5,
-    y = 0.5,
-    gp = gpar(
-      fontsize = 20,
-      fontface = "bold",
-      col = "black"
-    )
-  )
-}
+# ----- empty plot ------
+# need to add ggtext to make this nicer
+empty_plot <- function(msg) {
+  ggplot() +
+    theme_void() +
+    annotate("rect",
+             xmin = 0.1, xmax = 0.9,
+             ymin = 0.4, ymax = 0.6,
+             fill = "#ffffff",
+             color = "#2196F3",
+             linewidth = 2) +
+    annotate("rect",
+             xmin = 0.1, xmax = 0.13,
+             ymin = 0.4, ymax = 0.6,
+             fill = "#2196F3",
+             color = NA) +
+    annotate("point",
+             x = 0.17, y = 0.5,
+             size = 12,
+             color = "#2196F3") +
+    annotate("text",
+             x = 0.17, y = 0.5,
+             label = "i",
+             size = 8,
+             color = "white",
+             fontface = "bold") +
+    annotate("text",
+             x = 0.23, y = 0.5,
+             label = msg,
+             size = 6,
+             color = "#333333",
+             hjust = 0) +
+    xlim(0, 1) +
+    ylim(0, 1)
 
-
-empty_scatter_message <- function(msg) {
-  ggplot2::ggplot() +
-    ggplot2::annotate(
-      "text",
-      x = 0.5, y = 0.5,
-      label = msg,
-      size = 6,
-      fontface = "bold",
-      hjust = 0.5,
-      vjust = 0.5
-    ) +
-    ggplot2::theme_void()
 }
