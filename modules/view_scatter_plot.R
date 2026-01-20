947ff7e3b63fee50d75c0bdfcca83d9410e9c699
benjaminhlina <benjamin.hlina@gmail.com>
2026-01-19 19:22:09 -0500

    Remove observeEvent code

--- modules/view_scatter_plot.R
+++ modules/view_scatter_plot.R
@@ -64,10 +64,6 @@ scatter_plot_server <- function(id, con, main_input, scatter_sidebar_vals) {
     display_scatter_plot(data = filtered_scatter_data,
                          input_source = scatter_sidebar_vals,
                          output)
-
-  # },
-  # ignoreInit = TRUE
-  # )
   }
   )
 }
