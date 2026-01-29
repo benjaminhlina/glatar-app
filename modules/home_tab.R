7c84392f0bfe115aa5fbe5add1bb6f29de944d47
benjaminhlina <benjamin.hlina@gmail.com>
2026-01-29 08:51:29 -0500

    Change to V14

--- modules/home_tab.R
+++ modules/home_tab.R
@@ -47,8 +47,8 @@ home_tab_ui <- function(id) {
             tags$span("Data entry templates can be found by "),
             tags$a(
               "clicking here",
-              href = "data-entry-template/GLATAR_data_entry_template_v13.xlsx",
-              download = "GLATAR_data_entry_template_v13.xlsx"
+              href = "data-entry-template/GLATAR_data_entry_template_v14.xlsx",
+              download = "GLATAR_data_entry_template_v14.xlsx"
             ),
             tags$span(" and in the documentation pane.")
           ),
