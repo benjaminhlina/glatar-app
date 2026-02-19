3bb831879ac11057e4da1f2e549d36b5794132f1
benjaminhlina <benjamin.hlina@gmail.com>
2026-02-19 09:43:36 -0500

    Add fluid row - with templates links

--- modules/document_ui.R
+++ modules/document_ui.R
@@ -11,13 +11,27 @@ docs_ui <- function(id) {
 
         tags$p("Download the data entry templates and documentation below:"),
 
-        tags$ul(
-          tags$li(
-            tags$a(
-              "GLATAR Data Entry Template (v15)",
-              href = "data-entry-template/GLATAR_data_entry_template_v15.xlsx",
-              download = "GLATAR_data_entry_template_v15.xlsx",
-              target = "_blank"
+        fluidRow(
+
+          # ---- Templates column ----
+          column(
+            width = 6,
+            tags$h4("Templates"),
+
+            tags$ul(
+              tags$li(
+                tags$a(
+                  "GLATAR Data Entry Template (v15)",
+                  href = "data-entry-template/GLATAR_data_entry_template_v15.xlsx",
+                  download = "GLATAR_data_entry_template_v15.xlsx",
+                  target = "_blank"
+                )
+              )
+
+              # add more templates here
+            )
+          ),
+
             )
           )
         )
