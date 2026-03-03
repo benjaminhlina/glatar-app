b164f23c1cec99f60eff323bb2238c438eb14310
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-02 19:54:45 -0500

    add back in sslmode

--- modules/start_db_connection.R
+++ modules/start_db_connection.R
@@ -5,7 +5,7 @@
 
 # ----- create connection to database -----
 
-sslmode <- Sys.getenv("POSTGRES_SSLMODE", unset = "disable")
+ssl_mode <- Sys.getenv("POSTGRES_SSLMODE", unset = "disable")
 
 cli::cli_alert_info("The SSL mode is {.val {sslmode}}")
 cli::cli_alert("Starting db connection")
@@ -16,7 +16,7 @@ con <- DBI::dbConnect(RPostgres::Postgres(),
                       port = Sys.getenv("POSTGRES_PORT"),
                       user = Sys.getenv("POSTGRES_USER"),
                       password = Sys.getenv("POSTGRES_PASSWORD"),
-                      sslmode = "disable"
+                      sslmode = ssl_mode
                       )
 
 cli::cli_alert_success("db successfully connected to {.val {Sys.getenv('POSTGRES_DB')}} on {.val {Sys.getenv('POSTGRES_HOST')}}")
