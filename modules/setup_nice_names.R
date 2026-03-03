e7419d31f407173c4008d2fa0026e18a7fc2a0de
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-03 16:07:35 -0500

    Go back to reading csv this was dumb

--- modules/setup_nice_names.R
+++ modules/setup_nice_names.R
@@ -1,23 +1,28 @@
+{
+  library(dplyr)
+  library(here)
+  library(readr)
+}
+
 # ---- create renaming table ----
 
-get_nice_name_lookup <- function(con) {
-  naming_conventions <- tbl(con, "tbl_naming_conventions") |>
-    collect() |>
-    mutate(
-      nice_names = case_when(
-        raw_names %in% "d13c" ~ "\U03B4<sup>13</sup>C",
-        raw_names %in% "d15n" ~ "\U03B4<sup>15</sup>N",
-        raw_names %in% "d34s" ~ "\U03B4<sup>34</sup>S",
-        .default = nice_names
-      )
+naming_conventions <- read_csv(here::here(
+  "data",
+  "app_naming_conventions.csv"
+)) |>
+  mutate(
+    nice_names = case_when(
+      raw_names %in% "d13c" ~ "\U03B4<sup>13</sup>C",
+      raw_names %in% "d15n" ~ "\U03B4<sup>15</sup>N",
+      raw_names %in% "d34s" ~ "\U03B4<sup>34</sup>S",
+      .default = nice_names
     )
-
-  # print(naming_conventions, n = 50)
-
-  # create named vectors
-  nice_names <- setNames(
-    naming_conventions$nice_names,
-    naming_conventions$raw_names
   )
-  return(nice_names)
-}
+
+# print(naming_conventions, n = 50)
+
+# create named vectors
+nice_name_lookup <- setNames(
+  naming_conventions$nice_names,
+  naming_conventions$raw_names
+)
