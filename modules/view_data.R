6de040ac1669f9b157ce7af1a57dba1caba25dfc
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-15 10:44:04 -0400

    Comment out old method

--- modules/view_data.R
+++ modules/view_data.R
@@ -13,45 +13,53 @@ view_data_ui <- function(id) {
 # ---- server ----
 view_data_server <- function(id, con, main_input, raw_sidebar_vals) {
   moduleServer(id, function(input, output, session) {
-    # Get all table names and update selectInput dynamically
-    observe({
-      table_names <- DBI::dbListTables(con)
-
-      table_names <- setdiff(table_names, "tbl_submission")
-
-      updateSelectInput(session, "table_select", choices = table_names)
-    })
-
-    # Render the selected table
-    output$selected_table <- DT::renderDT(
+    observeEvent(
+      main_input$tabs,
       {
-        allowed_tables <- c(
-          "tbl_amino_acid",
-          "tbl_calorimetry",
-          "tbl_contaminants",
-          "tbl_data_dictionary",
-          "tbl_fatty_acid",
-          "tbl_isotope",
-          "tbl_length",
-          "tbl_lipid_composition",
-          "tbl_location",
-          "tbl_proxcomp",
-          "tbl_samples",
-          "tbl_source",
-          "tbl_taxonomy",
-          "tbl_thiamine"
+        shinyjs::toggle(
+          id = "raw_data_ui",
+          condition = main_input$tabs == "view_data"
         )
-
-        req(input$table_select %in% allowed_tables)
-
-        safe_name <- DBI::dbQuoteIdentifier(con, input$table_select)
-
-        DBI::dbGetQuery(con, paste0("SELECT * FROM ", safe_name))
       },
-      options = list(
-        pageLength = 15,
-        scrollX = TRUE
-      )
+      ignoreInit = TRUE
     )
+
+    # ---- namespaces -----
+    ns <- session$ns
+
+    #   # Get all table names and update selectInput dynamically
+    #   observe({
+    #     table_names <- DBI::dbListTables(con)
+    #     table_names <- setdiff(table_names, "tbl_submission")
+    #     updateSelectInput(session, "table_select", choices = table_names)
+    #   })
+    #   # Render the selected table
+    #   output$selected_table <- DT::renderDT(
+    #     {
+    #       allowed_tables <- c(
+    #         "tbl_amino_acid",
+    #         "tbl_calorimetry",
+    #         "tbl_contaminants",
+    #         "tbl_data_dictionary",
+    #         "tbl_fatty_acid",
+    #         "tbl_isotope",
+    #         "tbl_length",
+    #         "tbl_lipid_composition",
+    #         "tbl_location",
+    #         "tbl_proxcomp",
+    #         "tbl_samples",
+    #         "tbl_source",
+    #         "tbl_taxonomy",
+    #         "tbl_thiamine"
+    #       )
+    #       req(input$table_select %in% allowed_tables)
+    #       safe_name <- DBI::dbQuoteIdentifier(con, input$table_select)
+    #       DBI::dbGetQuery(con, paste0("SELECT * FROM ", safe_name))
+    #     },
+    #     options = list(
+    #       pageLength = 15,
+    #       scrollX = TRUE
+    #     )
+    #   )
   })
 }
