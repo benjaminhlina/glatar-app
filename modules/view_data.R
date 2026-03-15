3b1667a1e58309f8c52f118503feaa49140d6cc0
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-15 14:45:24 -0400

    remove old way and use new way

--- modules/view_data.R
+++ modules/view_data.R
@@ -44,39 +44,61 @@ view_data_server <- function(id, con, main_input, raw_sidebar_vals) {
     # ---- namespaces -----
     ns <- session$ns
 
-    #   # Get all table names and update selectInput dynamically
-    #   observe({
-    #     table_names <- DBI::dbListTables(con)
-    #     table_names <- setdiff(table_names, "tbl_submission")
-    #     updateSelectInput(session, "table_select", choices = table_names)
-    #   })
-    #   # Render the selected table
-    #   output$selected_table <- DT::renderDT(
-    #     {
-    #       allowed_tables <- c(
-    #         "tbl_amino_acid",
-    #         "tbl_calorimetry",
-    #         "tbl_contaminants",
-    #         "tbl_data_dictionary",
-    #         "tbl_fatty_acid",
-    #         "tbl_isotope",
-    #         "tbl_length",
-    #         "tbl_lipid_composition",
-    #         "tbl_location",
-    #         "tbl_proxcomp",
-    #         "tbl_samples",
-    #         "tbl_source",
-    #         "tbl_taxonomy",
-    #         "tbl_thiamine"
-    #       )
-    #       req(input$table_select %in% allowed_tables)
-    #       safe_name <- DBI::dbQuoteIdentifier(con, input$table_select)
-    #       DBI::dbGetQuery(con, paste0("SELECT * FROM ", safe_name))
-    #     },
-    #     options = list(
-    #       pageLength = 15,
-    #       scrollX = TRUE
-    #     )
-    #   )
+    # reactive export df
+    raw_export_df <- reactiveVal(NULL)
+
+    # reactive when raw is actived
+    raw_activated <- reactiveVal(FALSE)
+
+    #  ----- first create raw data -----
+    # raw actived_true only if
+    observeEvent(
+      main_input$tabs,
+      {
+        req(main_input$tabs == "view_data")
+        raw_activated(TRUE)
+      },
+      ignoreInit = TRUE
+    )
+
+    # create summary data
+    raw_data <- create_raw_data(
+      con = con,
+      main_input = main_input,
+      input_source = raw_sidebar_vals,
+      tab = "view_data",
+      var_field = "y_variable",
+      activated = raw_activated()
+    )
+
+    # filtered summary by waterbody and species
+    filtered_raw_data <- create_filtered_data(
+      input_source = raw_sidebar_vals,
+      data = raw_data,
+      pane = "view_data"
+    )
+
+    filtered_raw_data_df_names <- reactive({
+      req(filtered_raw_data())
+
+      filtered_raw_data() |>
+        dplyr::rename_with(~ convert_nice_name(.x))
+    })
+
+    display_table(
+      data = filtered_raw_data_df_names,
+      output,
+      output_id = "raw_data_output"
+    )
+
+    # ---- run exporte -----
+    observe({
+      raw_export_df(filtered_raw_data_df_names())
+    })
+
+    # ----- return this so it can be exported -----
+    return(list(
+      raw_df = filtered_raw_data_df_names
+    ))
   })
 }
