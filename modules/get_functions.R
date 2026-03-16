499ec5dfa77875dcb3024c72cd13503a0ef358fd
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-16 09:16:03 -0400

    get_id_max has been moved to get_functions which makes sense and is where it should have always been

--- modules/get_functions.R
+++ modules/get_functions.R
@@ -89,6 +89,15 @@ get_groups <- function(df) {
   return(groups)
 }
 
+# ----- get max id ----- 
+get_id_max <- function(table_name, id_col) {
+  result <- DBI::dbGetQuery(
+    con,
+    glue::glue("SELECT COALESCE(MAX({id_col}), 0) AS max_id FROM {table_name}")
+  )
+  selected_id_max <- result$max_id
+  return(selected_id_max)
+}
 
 # ----- simple function to get a tb use dbplyr -----
 #
