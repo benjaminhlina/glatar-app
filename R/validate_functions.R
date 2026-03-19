e3f0ef9082626747874631914a5dd2b953a5110a
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-19 15:55:38 -0400

    fix is.na check location and add"numeric__"

--- R/validate_functions.R
+++ R/validate_functions.R
@@ -116,8 +116,6 @@ pretty_validate_report <- function(confrontation, table_name = NULL) {
         grepl("is.na\\(as.Date", expression) ~ "Date format does not follow the
         required format of yyyy-mm-dd or is an invalid date",
 
-        # grepl("is.na", expression) ~ "Required field - cannot be empty",
-        grepl("numeric__", name) ~ "Field needs to be numeric",
         grepl("is.na", expression) ~ "Required field - cannot be empty",
         grepl("common_name", expression) ~ "Common name not found in database",
         grepl("scientific_name", expression) ~ "Scientific name not found in
