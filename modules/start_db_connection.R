188d0d0edfc932561c1cc5dcf867df0a8393d72e
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-18 22:53:56 -0400

    move statup dba into a function

--- modules/start_db_connection.R
+++ modules/start_db_connection.R
@@ -1,23 +1,26 @@
 # ----- create connection to database -----
-cli::cli_alert_info("The SSL mode is {.val {Sys.getenv('POSTGRES_SSLMODE')}}")
-ssl_mode <- Sys.getenv("POSTGRES_SSLMODE", unset = "disable")
+start_db_con <- function() {
+  cli::cli_alert_info("The SSL mode is {.val {Sys.getenv('POSTGRES_SSLMODE')}}")
+  ssl_mode <- Sys.getenv("POSTGRES_SSLMODE", unset = "disable")
 
-cli::cli_alert_info("The SSL mode is {.val {ssl_mode}}")
-cli::cli_alert("Starting db connection")
+  cli::cli_alert_info("The SSL mode is {.val {ssl_mode}}")
+  cli::cli_alert("Starting db connection")
 
-con <- DBI::dbConnect(
-  RPostgres::Postgres(),
-  dbname = Sys.getenv("POSTGRES_DB"),
-  host = Sys.getenv("POSTGRES_HOST"),
-  port = Sys.getenv("POSTGRES_PORT"),
-  user = Sys.getenv("POSTGRES_USER"),
-  password = Sys.getenv("POSTGRES_PASSWORD"),
-  sslmode = ssl_mode
-)
+  con <- DBI::dbConnect(
+    RPostgres::Postgres(),
+    dbname = Sys.getenv("POSTGRES_DB"),
+    host = Sys.getenv("POSTGRES_HOST"),
+    port = Sys.getenv("POSTGRES_PORT"),
+    user = Sys.getenv("POSTGRES_USER"),
+    password = Sys.getenv("POSTGRES_PASSWORD"),
+    sslmode = ssl_mode
+  )
 
-cli::cli_alert_success(
-  "db successfully connected to {.val {Sys.getenv('POSTGRES_DB')}} on {.val {Sys.getenv('POSTGRES_HOST')}}"
-)
-cli::cli_alert_success(
-  "db successfully connected (is_valid: {.val {DBI::dbIsValid(con)}})"
-)
+  cli::cli_alert_success(
+    "db successfully connected to {.val {Sys.getenv('POSTGRES_DB')}} on {.val {Sys.getenv('POSTGRES_HOST')}}"
+  )
+  cli::cli_alert_success(
+    "db successfully connected (is_valid: {.val {DBI::dbIsValid(con)}})"
+  )
+  return(con)
+}
