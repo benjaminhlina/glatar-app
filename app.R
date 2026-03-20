c56231ab5cedbe2288ade4ad5a0740e91aa469c3
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-19 21:08:19 -0400

    move this to secure functions

--- app.R
+++ app.R
@@ -135,44 +135,10 @@ ui <- shinydashboard::dashboardPage(
   )
 )
 
-# ---- make this look nicer -----
-ui <- shinymanager::secure_app(
+# ---- secure ui -----
+ui <- secure_ui(
   ui,
-  enable_admin = FALSE,
-  # Bootstrap flatly, cerulean, cosmo,
-  theme = "flatly",
-  language = "en",
-  timeout = 15.0,
-  fab_position = "none",
-  # Customize the login page appearance
-  tags_top = shiny::tags$div(
-    shiny::tags$h2(
-      "Great Lakes Aquatic Tissue Analysis Repository (GLATAR)",
-      style = "text-align: center; color: #2c3e50; margin-bottom: 20px;"
-    ),
-    shiny::tags$img(
-      src = "logo/glfc-logo.png",
-      width = 150,
-      style = "display: block; margin: 0 auto 20px auto;"
-    )
-  ),
-
-  tags_bottom = shiny::tags$div(
-    shiny::tags$p(
-      "Please login to access the application",
-      style = "text-align: center; color: #7f8c8d;"
-    )
-  ),
-
-  # Customize button colors and text
-  choose_language = FALSE,
-  lan = list(
-    en = list(
-      title = "Please Authenticate",
-      user = "Email Address",
-      password = "Password"
-    )
-  )
+  # off = TRUE
 )
 
 server <- function(input, output, session) {
@@ -181,8 +147,9 @@ server <- function(input, output, session) {
   ram_tracker()
   session$allowReconnect("force")
 
-  res_auth <- shinymanager::secure_server(
-    check_credentials = shinymanager::check_credentials(credentials),
+  res_auth <- authorize_server(
+    credentials = credentials,
+    # off = TRUE
   )
 
   # ---- add in logout tab -----
