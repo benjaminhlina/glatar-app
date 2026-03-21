3e91895a72adfd3bd85397b25d4c50e882895b97
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-20 23:19:27 -0400

    Create upload_functions

--- modules/upload_data.R
+++ modules/upload_data.R
@@ -345,62 +345,10 @@ upload_data_server <- function(id, con) {
       # --- assume tables_split_full is a named list ---
       tables_to_submit <- tables_to_submit()
 
-      # Create a list to store submission results
-      submission_results <- list()
-
       # ----- begiging concection -----
-      DBI::dbBegin(con)
-
-      # keep looop but now wrap in try catch so that it won't let you
-      # submit if this errors
-      upload_succeeded <- tryCatch(
-        {
-          for (tbl_name in names(tables_to_submit)) {
-            df <- tables_to_submit[[tbl_name]]
-
-            if (nrow(df) > 0) {
-              cli::cli_alert_info(
-                "Submitting table: {tbl_name} with {nrow(df)} rows..."
-              )
-
-              DBI::dbAppendTable(con, tbl_name, df) # all writes inside the single transaction
-
-              cli::cli_alert_success("{tbl_name} submitted successfully")
-              submission_results[[tbl_name]] <- list(
-                rows_submitted = nrow(df),
-                submission_id = if ("submission_id" %in% colnames(df)) {
-                  unique(df$submission_id)
-                } else {
-                  NA
-                }
-              )
-            } else {
-              submission_results[[tbl_name]] <- list(
-                rows_submitted = 0,
-                submission_id = NA
-              )
-              cli::cli_alert_info("{tbl_name} has no rows to submit, skipping.")
-            }
-          }
-
-          DBI::dbCommit(con)
-          shiny::showNotification("Upload successful!", type = "message")
-          TRUE
-        },
-        error = function(e) {
-          # if errors it will rolle back and display an alert
-          DBI::dbRollback(con)
-          cli::cli_alert_danger(
-            "Upload failed due to inconsistances,
-                              rolled back: {e$message}"
-          )
-          shiny::showNotification(
-            "Upload failed. No data was saved.",
-            type = "error"
-          )
-          FALSE
-        }
-      )
+      upload_result <- upload_to_db(con, tables_to_submit = tables_to_submit)
+      upload_succeeded <- upload_result$succeeded
+      submission_results <- upload_result$results
 
       # Create a message to display
       output$upload_status <- shiny::renderUI({
