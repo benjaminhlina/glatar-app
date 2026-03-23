1be2524514dfab114ec2d51e63e2e5101a56353f
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-23 12:01:56 -0400

    We have remove on.exit as this was being rerturn resutlign in nothign dispalying load_incitor hoide is now ohw this is being handled

--- R/load_indicator.R
+++ R/load_indicator.R
@@ -1,3 +1,4 @@
+# ----- load oinidciator -----
 load_indicator <- function(input, output) {
   shiny::req(input$file_upload)
   shinyjs::show("loading_indicator")
@@ -5,8 +6,8 @@ load_indicator <- function(input, output) {
   shinyjs::disable("upload_btn")
 }
 
-  on.exit({
-    shinyjs::hide("loading_indicator")
-    shinyjs::enable("upload_btn")
-  })
+# ----- hide ------
+load_indicator_hide <- function(input, output) {
+  shinyjs::hide("loading_indicator")
+  shinyjs::enable("upload_btn")
 }
