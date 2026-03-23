0eda45e608860c1ec9ffc9d8164fbc044575a307
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-23 12:35:04 -0400

    switchin tto html from shinyjs load_message

--- R/load_indicator.R
+++ R/load_indicator.R
@@ -2,12 +2,13 @@
 load_indicator <- function(input, output) {
   shiny::req(input$file_upload)
   shinyjs::show("loading_indicator")
-  output$loading_msg <- shiny::renderText("Processing file, please wait...")
+  shinyjs::html("loading_msg", "Processing file, please wait...")
   shinyjs::disable("upload_btn")
 }
 
 # ----- hide ------
 load_indicator_hide <- function(input, output) {
   shinyjs::hide("loading_indicator")
+  shinyjs::html("loading_msg", "")
   shinyjs::enable("upload_btn")
 }
