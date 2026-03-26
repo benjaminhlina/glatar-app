c83b449ef17e6d9b02210491da88a419711fce44
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-26 10:29:50 -0400

    fix html

--- modules/glatar_diagram.R
+++ modules/glatar_diagram.R
@@ -570,7 +570,7 @@ glatar_diagram_ui <- function() {
         a.href = docPath;
         a.target = "_blank";
         a.rel = "noopener noreferrer";
-        a.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/></svg>' + "View Documentation";
+        a.innerHTML = "<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/></svg>" + "View Documentation";
       docEl.appendChild(a);
     });
     } else {
