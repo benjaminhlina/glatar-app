8f9227427129a91ad052fbb7f3083fd0dbd4980a
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-27 07:39:36 -0400

    remove change of old reules to them being handled by rules_issue

--- R/validate_functions.R
+++ R/validate_functions.R
@@ -113,13 +113,8 @@ pretty_validate_report <- function(confrontation, table_name = NULL) {
           " - you have altered the data entry template -
           please reupload an unaltered file"
         ),
-        grepl("is.na\\(as.Date", expression) ~ "Date format does not follow the
-        required format of yyyy-mm-dd or is an invalid date",
         grepl("numeric__", name) ~ "Field needs to be numeric",
-        
-        grepl("common_name", expression) ~ "Common name not found in database",
-        grepl("scientific_name", expression) ~ "Scientific name not found in
-        database",
+
         !is.na(rule_issue) ~ rule_issue,
 
         grepl("is.na", expression) ~ "Required field - cannot be empty",
