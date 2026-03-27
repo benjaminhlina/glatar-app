13c37412f74cecb95a7c0ec5406cce6d8511ee0d
Benjamin Hlina <benjamin.hlina@gmail.com>
2026-03-26 22:03:00 -0400

    Move the order in which validator info was being grepl within case when block

--- R/validate_functions.R
+++ R/validate_functions.R
@@ -116,14 +116,12 @@ pretty_validate_report <- function(confrontation, table_name = NULL) {
         grepl("is.na\\(as.Date", expression) ~ "Date format does not follow the
         required format of yyyy-mm-dd or is an invalid date",
         grepl("numeric__", name) ~ "Field needs to be numeric",
-        grepl("is.na", expression) ~ "Required field - cannot be empty",
-        grepl("numeric__", name) ~ "Field needs to be numeric",
         grepl("common_name", expression) ~ "Common name not found in database",
         grepl("scientific_name", expression) ~ "Scientific name not found in
         database",
         !is.na(rule_issue) ~ rule_issue,
-
-        # !!!validation_rules,
+        lgrepl("is.na", expression) ~ "Required field - cannot be empty",
+      
         .default = expression
       ),
     ) |>
