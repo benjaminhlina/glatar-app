86123d5854a61464fbb875d640ef1caa26f22bba
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-30 10:35:12 -0400

    Update info on documnets for templates

--- modules/document_ui.R
+++ modules/document_ui.R
@@ -18,23 +18,34 @@ docs_ui <- function(id) {
           shiny::column(
             width = 6,
             shiny::tags$h4("Templates"),
-
             shiny::tags$ul(
               shiny::tags$li(
+                style = "white-space: nowrap;",
                 shiny::tags$a(
                   "Basic Data Entry Template (v21)",
                   href = "data-entry-template/GLATAR_data_entry_template_v21.xlsx",
                   download = "GLATAR_data_entry_template_v21.xlsx",
                   target = "_blank"
+                ),
+                shiny::tags$span(
+                  " - collects energy density, 
+                  proximate composition, and stable isotope data.",
+                  style = "display: inline;"
                 )
               ),
               # add more templates here
               shiny::tags$li(
+                style = "white-space: nowrap;",
                 shiny::tags$a(
                   "Extended Data Entry Template (v21)",
                   href = "data-entry-template/GLATAR_data_entry_template_v21_long.xlsx",
                   download = "GLATAR_data_entry_template_v21_long.xlsx",
-                  target = "_blank"
+                  target = "_blank",
+                  style = "display: inline;"
+                ),
+                shiny::tags$span(
+                  " - additional fields to collect thiamine, fatty and amino acid, mercury, and PCB data.",
+                  style = "display: inline;"
                 )
               )
 
