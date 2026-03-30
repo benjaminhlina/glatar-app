b3de3d0b32312cb0e7a5faa3bb07e5a972cfe64d
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-30 10:41:43 -0400

    Move display functions to R folder and remove validation message

--- modules/validation_message.R
+++
@@ -1,7 +0,0 @@
-# ---- make validation message ----
-make_validation_message <- function(column, message) {
-  rlang::new_formula(
-    lhs = rlang::expr(grepl(!!paste0("\\.", column), expression)),
-    rhs = message
-  )
-}
