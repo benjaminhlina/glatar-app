3cdf7a5682f087ae510ca6fb4d3e558337e6d8ab
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-30 12:59:17 -0400

    Intial commit of view source

---
+++ modules/view_source.R
@@ -0,0 +1,42 @@
+view_source_ui <- function(id) {
+  ns <- shiny::NS(id)
+
+  shiny::tagList(
+    shinyjs::useShinyjs(),
+    shiny::div(
+      id = ns("source_ui"),
+      style = "display:none;",
+      shiny::h2("Source Material"),
+      shiny::p(
+        "This panel displays information on source material supporting the data that is in GLATAR."
+      ),
+      shiny::fluidRow(
+        shinydashboard::box(
+          title = "Source Material Table",
+          status = "primary",
+          solidHeader = TRUE,
+          width = 12,
+          div(
+            style = "overflow-x: auto; width: 100%;",
+            DT::DTOutput(ns("source_output"))
+          )
+        )
+      )
+    )
+  )
+}
+
+view_source_server <- function(id, main_input) {
+  shiny::moduleServer(id, function(input, output, session) {
+    shiny::observeEvent(
+      main_input$tabs,
+      {
+        shinyjs::toggle(
+          id = "source_ui",
+          condition = main_input$tabs == "view_source"
+        )
+      },
+      ignoreInit = TRUE
+    )
+  })
+}
