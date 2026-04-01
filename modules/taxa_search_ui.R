c724b2ae27136b8bf69a28d10a913213a2cd9646
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-31 20:11:41 -0400

    push everything in create_Searching_data()

--- modules/taxa_search_ui.R
+++ modules/taxa_search_ui.R
@@ -19,42 +19,12 @@ taxa_search_ui <- function(id) {
 }
 taxa_search_server <- function(id, con) {
   shiny::moduleServer(id, function(input, output, session) {
-    filtered_taxa <- shiny::reactive({
-      search_cols <- dplyr::tbl(con, "tbl_taxonomy") |>
-        colnames()
-
-      df <- dplyr::tbl(con, "tbl_taxonomy") |>
-        dplyr::arrange(common_name)
-
-      search_term <- input$search_bar
-
-      # Only filter if the user has typed something
-      if (!is.null(search_term) && nzchar(trimws(search_term))) {
-        pattern <- paste0("%", search_term, "%")
-        conditions <- purrr::reduce(
-          purrr::map(
-            search_cols,
-            ~ rlang::expr(as.character(!!rlang::sym(.x)) %ilike% !!pattern)
-          ),
-          ~ rlang::expr(!!.x | !!.y)
-        )
-
-        df <- df |>
-          dplyr::filter(!!conditions)
-      }
-
-      df <- df |>
-        dplyr::collect()
-      current_names <- names(df)
-
-      names(df) <- dplyr::coalesce(
-        nice_name_lookup[current_names],
-        current_names
-      )
-
-      return(df)
-    })
-
+    filtered_taxa <- create_searching_data(
+      con = con,
+      tbl_name = "tbl_taxonomy",
+      input = input,
+      collect = TRUE
+    )
     output$taxa_table <- DT::renderDT({
       DT::datatable(
         filtered_taxa(),
