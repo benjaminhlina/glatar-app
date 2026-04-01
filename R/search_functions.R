de1a2bdb7e4433a96bdb2b414f268fd8e5b62565
benjaminhlina <benjamin.hlina@gmail.com>
2026-03-31 20:39:38 -0400

    Add in per term seraching

--- R/search_functions.R
+++ R/search_functions.R
@@ -1,26 +1,32 @@
 # ---- search table ------
 search_table <- function(df, search_term, search_cols) {
-  if (inherits(df, "tbl_lazy")) {
-    pattern <- paste0("%", search_term, "%")
-    conditions <- purrr::reduce(
-      purrr::map(
-        search_cols,
-        ~ rlang::expr(as.character(!!rlang::sym(.x)) %ilike% !!pattern)
-      ),
-      ~ rlang::expr(!!.x | !!.y)
-    )
-  } else {
-    pattern <- search_term
-    conditions <- purrr::reduce(
-      purrr::map(
-        search_cols,
-        ~ rlang::expr(
-          grepl(!!pattern, as.character(!!rlang::sym(.x)), ignore.case = TRUE)
-        )
-      ),
-      ~ rlang::expr(!!.x | !!.y)
-    )
-  }
+  terms <- stringr::str_split(search_term, "\\s+")[[1]]
+  terms <- terms[nzchar(terms)]
+
+  per_term_conditions <- purrr::map(terms, function(term) {
+    if (inherits(df, "tbl_lazy")) {
+      pattern <- paste0("%", term, "%")
+      conditions <- purrr::reduce(
+        purrr::map(
+          search_cols,
+          ~ rlang::expr(as.character(!!rlang::sym(.x)) %ilike% !!pattern)
+        ),
+        ~ rlang::expr(!!.x | !!.y)
+      )
+    } else {
+      purrr::reduce(
+        purrr::map(
+          search_cols,
+          ~ rlang::expr(
+            grepl(!!term, as.character(!!rlang::sym(.x)), ignore.case = TRUE)
+          )
+        ),
+        ~ rlang::expr(!!.x | !!.y)
+      )
+    }
+  })
+
+  conditions <- purrr::reduce(per_term_conditions, ~ rlang::expr(!!.x & !!.y))
 
   df <- df |>
     dplyr::filter(!!conditions)
