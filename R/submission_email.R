4cf9dd7f07d1c9ee41c4daedb50de81f5bc8107a
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-03 18:45:34 -0400

    Add reply disclaimer

--- R/submission_email.R
+++ R/submission_email.R
@@ -22,6 +22,10 @@ email_body_html <- function(submission_id, submission_results) {
     <p>If you have questions, please contact the GLATAR manager at
     <a href="mailto:benjamin.hlina@gmail.com">benjamin.hlina@gmail.com</a>.</p>
     <p>— GLATAR Team</p>
+
+    <p><em>Please do not reply to this email as it will not be received. 
+    If you have any concerns, please email <a href="mailto:benjamin.hlina@gmail.com">benjamin.hlina@gmail.com</a> with 
+    your submission ID in the subject line.</em></p>
   '
   )
 }
