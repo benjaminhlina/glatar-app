042d08ca9a2f078a5400d9f5884087dbf7cc9ae5
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-07 13:07:51 -0400

    remove commented line

--- modules/plot_ui.R
+++ modules/plot_ui.R
@@ -8,7 +8,6 @@ plot_ui <- function(title, plot_id, height, ...) {
       status = "info",
       solidHeader = TRUE,
       width = 12,
-      # plotlyOutput("summary_histogram", height = "300px")
       shinycssloaders::withSpinner(
         shiny::plotOutput(ns(plot_id), height = height),
         type = 4,
