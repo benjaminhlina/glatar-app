2c0bcb2731c07b9b8d417af35a4250728a0ec860
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-07 15:51:20 -0400

    ADD IN PLOT CONTROSL

--- modules/view_scatter_plot.R
+++ modules/view_scatter_plot.R
@@ -21,6 +21,19 @@ view_scatter_plot_ui <- function(id) {
         plot_id = "scatter_plot",
         height = "600px",
         ns = ns
+      ),
+      shiny::hr(),
+      shiny::h4("Plot Controls", style = "margin-top: 5px;"),
+      shiny::fluidRow(
+        shiny::column(4, pallete_selector(ns = ns)),
+        shiny::column(
+          4,
+          zoom_slider_ui(ns = ns, label = "Zoom X Axis", id = "zoom_x")
+        ),
+        shiny::column(
+          4,
+          zoom_slider_ui(ns = ns, label = "Zoom Y Axis", id = "zoom_y")
+        )
       )
     )
   )
@@ -67,7 +80,7 @@ scatter_plot_server <- function(id, con, main_input, scatter_sidebar_vals) {
         "x_choices",
         "y_choices"
       ),
-      activated = scatter_activated()
+      activated = scatter_activated
     )
     # ---- allow filtering -----
     filtered_scatter_data <- create_filtered_data(
@@ -86,10 +99,58 @@ scatter_plot_server <- function(id, con, main_input, scatter_sidebar_vals) {
       ignoreInit = TRUE
     )
 
+    # ----- PLOT CONTROLS ------
+    palette_inputs <- pallete_selector_server(input)
+    zoom_inputs <- zoom_slider_server(input)
+
+    observeEvent(
+      {
+        filtered_scatter_data()
+        scatter_sidebar_vals$x_choices()
+        scatter_sidebar_vals$y_choices()
+      },
+      {
+        df <- filtered_scatter_data()
+        req(df)
+
+        var_map <- syn_var()
+
+        x_choice <- scatter_sidebar_vals$x_choices()
+        y_choice <- scatter_sidebar_vals$y_choices()
+
+        x_var <- var_map[[x_choice]] %||% x_choice
+        y_var <- var_map[[y_choice]] %||% y_choice
+
+        ranges <- get_ranges(df, x_var, y_var)
+
+        x_range <- ranges[["x_range_vec"]]
+        y_range <- ranges[["y_range_vec"]]
+        cli::cli_alert(
+          "x_range is: {.val {c(min = round(x_range[1]), max = round(x_range[2]))}}"
+        )
+        cli::cli_alert(
+          "y_range is: {.val {c(min = round(y_range[1]), max = round(y_range[2]))}}"
+        )
+
+        if (!is.null(x_range)) {
+          update_zoom_slider("zoom_x", x_range)
+        }
+        if (!is.null(y_range)) update_zoom_slider("zoom_y", y_range)
+      },
+      ignoreNULL = TRUE
+    )
+
+    # ----- CREATE INPUTS -----
+    input_sources <- c(
+      scatter_sidebar_vals,
+      palette_inputs,
+      zoom_inputs
+    )
+
     # ---- display plot -----
     display_scatter_plot(
       data = filtered_scatter_data,
-      input_source = scatter_sidebar_vals,
+      input_source = input_sources,
       output
     )
   })
