d15e95504c185b92df5b67bdeb88a3dc28bb0069
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-13 17:39:25 -0400

    Move this to plot_controls

--- R/fnct_plot_control.R
+++ R/fnct_plot_control.R
@@ -1,3 +1,23 @@
+plot_ui <- function(title, plot_id, height, ...) {
+  args <- list(...)
+  ns <- args$ns
+
+  shiny::fluidRow(
+    shinydashboard::box(
+      title = title,
+      status = "info",
+      solidHeader = TRUE,
+      width = 12,
+      shinycssloaders::withSpinner(
+        shiny::plotOutput(ns(plot_id), height = height),
+        type = 4,
+        caption = "Please wait for the plot to load..."
+      )
+    )
+  )
+}
+
+
 # ------ pallletes ------
 pallete_selector <- function(...) {
   args <- list(...)
