ba75c908f947221b7b367a2b50bfe0c25e3072c3
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-13 22:49:17 -0400

    Updated manual of glatar app

--- R/app.R
+++ R/app.R
@@ -1,4 +1,11 @@
-#' Run GLATAR app
+#' GLATAR Shiny App
+#'
+#' This function which has no arguments starts up the app,
+#' creates the user interface (UI) and server and combines them
+#' to make the app.
+#'
+#' @import shiny
+#' @import shinydashboard
 #' @export
 
 glatar_app <- function() {
