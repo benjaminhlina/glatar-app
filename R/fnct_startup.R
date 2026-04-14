dc1e42eb4eac02d1ceb0ce96fac2bd5a5c33dc60
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-13 23:30:02 -0400

    startup documenation

--- R/fnct_startup.R
+++ R/fnct_startup.R
@@ -1,6 +1,6 @@
 #' Start up GLATAR App
 #'
-#' This function which has no arguments starts the database
+#' This function, which has no arguments, starts the database
 #' connection, gets `valid_values`` for validation, gets
 #' `nice_names` for properly displaying tables, creates
 #' credientials and location for resources
