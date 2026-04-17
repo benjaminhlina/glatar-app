c6b0cb45e51fb721ea80883ad5b9b887f9e7cda6
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-17 17:18:07 -0400

    Edit start up info

--- R/fnct_startup.R
+++ R/fnct_startup.R
@@ -3,7 +3,7 @@
 #' This function, which has no arguments, starts the database
 #' connection, gets `valid_values`` for validation, gets
 #' `nice_names` for properly displaying tables, creates
-#' credientials and location for resources
+#' credientials, and and file location for resources.
 #'
 #' @export
 start_up <- function() {
@@ -42,5 +42,3 @@ start_up <- function() {
   )
   return(startup)
 }
-
-
