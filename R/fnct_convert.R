e66203ed4003411cdba4c19be11e3a91a1422c11
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-18 23:44:26 -0400

    add info on convert_nice_name

--- R/fnct_convert.R
+++ R/fnct_convert.R
@@ -1,4 +1,17 @@
-# ----- get nice names -----
+# ----- convert nice names -----
+
+#' Convert functions
+#'
+#' These functions convert columns whether that is column names or
+#' the values in the columns themeselves.
+#'
+#' @param cols column to be converted
+#' @param lookup a `vector` that has the name of the column
+#' and the nice looking name of the column e.g., `sample_id`
+#' becomes `Sample ID`.
+#'
+#' @name convert_functions
+#' @export
 convert_nice_name <- function(cols, lookup = nice_name_lookup) {
   converted_name <- unname(sapply(cols, function(col) {
     if (col %in% names(lookup)) {
