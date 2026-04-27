317a8e66ad4f04c84ab20d7f5c52ffd8ef782321
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-27 10:06:49 -0400

    load indicator is now documented

--- R/fnct_load_indicator.R
+++ R/fnct_load_indicator.R
@@ -1,4 +1,17 @@
 # ----- load oinidciator -----
+
+#' Load Indicator functions
+#'
+#'
+#' @param input a shiny `server` input value.
+#' @param output a shiny `server` output value.
+#'
+#' @details
+#' `load_indicator()` displays load indicator during file uploading
+#'
+#' @name load_indicator
+#' @export
+
 load_indicator <- function(input, output) {
   shiny::req(input$file_upload)
   shinyjs::show("loading_indicator")
@@ -7,6 +20,14 @@ load_indicator <- function(input, output) {
 }
 
 # ----- hide ------
+#' @param input a shiny `server` input value.
+#' @param output a shiny `server` output value.
+#'
+#' @details
+#' `load_indicator_hide()` displays load indicator during file uploading
+#'
+#' @name load_indicator
+#' @export
 load_indicator_hide <- function(input, output) {
   shinyjs::hide("loading_indicator")
   shinyjs::html("loading_msg", "")
