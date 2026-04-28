c75ff403f4d80c5fd92e4ff8e0bbb1ab6ee4347b
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-27 20:12:12 -0400

    ad observer event all documentation

--- R/fnct_observer.R
+++ R/fnct_observer.R
@@ -1,4 +1,20 @@
 # ----- filter observserer ----
+
+#' Exclude all observer
+#'
+#' This function uses an `observerEvent()` to adjust dropdown
+#' menus so that when other selections beside `"All"` is selected
+#' it will not allow `"All"` to be selected until the entire box
+#' is cleared.
+#'
+#'
+#' @param input a server input value.
+#' @param session a shiny session.
+#' @param id the dropdown object to apply the observer
+#'
+#' @return an Event that excludes `"All"``
+#' @export
+
 exclusive_all_observer <- function(input, session, id) {
   shiny::observeEvent(
     input[[id]],
