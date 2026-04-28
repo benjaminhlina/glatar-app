3412bd839833c5e266de583aca93c6520e7c82da
benjaminhlina <benjamin.hlina@gmail.com>
2026-04-28 18:11:07 -0400

    search tables documentation

--- R/fnct_search.R
+++ R/fnct_search.R
@@ -1,4 +1,19 @@
 # ---- search table ------
+#' Search functions
+#'
+#' Search functions allow for a dynamic search bar to be displayed to search
+#' taxa and source material
+#'
+#' @param df the data object being supplied can either be a `tbl_lazy` or `data.frame`.
+#' @param search_term a dynamic string that contains search terms
+#' @param search_cols a dynamic `vector` containing the names of the search
+#' columns
+
+#' @return returns a filtered data object that can then be displayed.
+#'
+#' @name search_functions
+#' @export
+
 search_table <- function(df, search_term, search_cols) {
   terms <- stringr::str_split(search_term, "\\s+")[[1]]
   terms <- terms[nzchar(terms)]
