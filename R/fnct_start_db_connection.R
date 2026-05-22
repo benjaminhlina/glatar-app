d6294f719aeb0a63025a34f4c0053cd6271be17a
benjaminhlina <benjamin.hlina@gmail.com>
2026-05-22 14:43:35 -0400

    add in pw for db con

--- R/fnct_start_db_connection.R
+++ R/fnct_start_db_connection.R
@@ -7,31 +7,41 @@
 #'
 #' @param username The username for user specific connections.
 #' Default is `NULL` and will use default user.
+#' @param password The passward for user specific connections.
+#' Default is `NULL` and will use default user.
 #'
 #' @return Returns a connection object.
 #'
 #' @name db_connections
 #' @export
 
-start_db_con <- function(username = NULL) {
+start_db_con <- function(username = NULL, password = NULL) {
   cli::cli_alert_info("The SSL mode is {.val {Sys.getenv('POSTGRES_SSLMODE')}}")
   ssl_mode <- Sys.getenv("POSTGRES_SSLMODE", unset = "disable")
 
   cli::cli_alert_info("The SSL mode is {.val {ssl_mode}}")
   cli::cli_alert("Starting db connection")
 
+  current_user <- if (!is.null(username)) {
+    username
+  } else {
+    Sys.getenv("POSTGRES_USER")
+  }
+  current_pw <- if (!is.null(password)) {
+    password
+  } else {
+    Sys.getenv("POSTGRES_PASSWORD")
+  }
+
   con <- DBI::dbConnect(
     RPostgres::Postgres(),
     dbname = Sys.getenv("POSTGRES_DB"),
     host = Sys.getenv("POSTGRES_HOST"),
     port = Sys.getenv("POSTGRES_PORT"),
-    if (!is.null(username)) {
-      user = username
-    } else {
-      user = Sys.getenv("POSTGRES_USER")
-    },
-    password = Sys.getenv("POSTGRES_PASSWORD"),
-    sslmode = ssl_mode
+    user = current_user,
+    password = current_pw,
+    sslmode = ssl_mode,
+    gssencmode = "disable"
   )
 
   cli::cli_alert_success(
